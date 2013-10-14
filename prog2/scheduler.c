70ce3c1fb2c8c338c78fa14ef1977fd7ee90adf0
Cameron Dunn <camdunn54321@gmail.com>
2013-10-14 11:00:58 -0700

    Did shit in class

--- prog2/scheduler.c
+++ prog2/scheduler.c
@@ -18,6 +18,8 @@
 #define MAX_ARGS 10
 #define MAX_PROGS 10
 
+const int quantum;
+
 //struct to build linked list of processes
 struct typedef proc_list_node{
 	int pid;
@@ -31,10 +33,7 @@ struct typedef proc_list_node{
 
 proc_list_node *curr;
 
-//dedicated function to quickly, and not so thouroughly
-// validate command-line arguments
-// Returns: 0 if all good. >0 not so good
-int arg_check(int, char, int);
+void printUsageAndExit() 
 //takes void* head pointer, argv, and delimiter to build 
 //linked list of processes. This function does the parsing,
 // forking (including sending stop signal)
@@ -52,50 +51,69 @@ int scheduler(proc_list_node*, int);
 int remove_node(proc_list_node*, int);
 
 
-int main(int argc, char *argv[]){
-  int i, quantum, err;
+int main(int argc, char *argv[]) {
   proc_list_node *head;
-
-  if(arg_check(argc, argv, &quantum)){
-  	 fprintf(stderr, "usage: schedule quantum [prog 1 [args] [: 
-  		prog 2 [args] [: prog3 [args] [: … ]]]]\n
-  	  \tquantum must be an integer greater than 0.\n
-  	  Maximum programs: %d. Maximum arguments per program: % d.\n",
-  	  MAX_PROGS, MAX_ARGS);
-  	 exit();
+  int progCount, argCount, parsingArgs = 0;
+  if (!(quantum = atol(argv[1]))) {
+    printUsageAndExit();
   }
-  assert(quantum > 0);
 
+  argv = argv + 2;
+
+  while (*argv) {
+    if (!parsingArgs) {
+      parsingArgs = 1;
+
+      proc_list_node *node = calloc(1, sizeof(proc_list_node));
+      argCount = 0;
+      progCount++;
+
+      if (progCount > MAX_PROGS) {
+        printUsageAndExit();
+      }
+
+      node->name = argv++;
+      node->args = argv;
+
+      if (head == NULL) {
+        node->next = head = curr = node;
+      } else {
+        curr = curr->next = node;
+        curr->next = head;
+      }
+    } else {
+      if (*argv == ':') {
+        *argv = NULL;
+        parsingArgs = 0;
+      } else {
+        argCount++;
+
+        if (argCount > MAX_ARGS) {
+          printUsageAndExit();
+        }
+      }
+
+      argv++;
+    }
+  }
+
+  curr = curr->next;
 
   return 0;
 }
 
-int arg_check(int argc, char *argv[], int *quantum){
-	int err = 0, arg_count = 0, prog_count = 0;
-	// immediate check if too many or too few args
-  if ( (argc > (MAX_PROGS * MAX_ARGS) + 2) || argc < 3) return(++err);
-  
-  // check if the 'quantem' value is valid. using atol() to
-  // determine if the value is a integer
-  if ( !(*quantum = atol(argv[1])) ) return(++err);
-
-  *argv++;
-  while(*argv++){
-  	if(**argv == ':'){
-  		prog_count++;	
-  		arg_count = 0;
-  	}
-  	if(arg_count++ > 10) return(++err);
-  }
-  if(prog_count > MAX_PROGS - 1) return(++err);
-
-  return err;
+void printUsageAndExit() {
+  fprintf(stderr, "usage: schedule quantum [prog 1 [args] [: 
+      prog 2 [args] [: prog3 [args] [: … ]]]]\n
+      \tquantum must be an integer greater than 0.\n
+      Maximum programs: %d. Maximum arguments per program: % d.\n",
+      MAX_PROGS, MAX_ARGS);
+  exit(1);
 }
 
-void* build_proc_list(void *list_head, char** argv, char *delimiter){
-	proc_list_node *head = calloc(1, sizeof(proc_list_node));
-	proc_list_node *temp = head;
 
+
+void forkFest() {
 
 }
 
